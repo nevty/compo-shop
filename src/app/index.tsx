254f7f3f2571b80e948c5599f4e8eeeb5bd05005
nevty <theacutemace@gmail.com>
2024-03-22 03:07:25 +0300

    feature: app main layout

---
+++ src/app/index.tsx
@@ -0,0 +1,25 @@
+import { IconContext } from "react-icons";
+import { Routes, Route } from "react-router-dom";
+
+import { HeaderWidget } from "@widgets/header";
+
+import { Navigation, Footer } from "./ui";
+
+function App() {
+  return (
+    <div className="font-montserrat text-sm">
+      <IconContext.Provider value={{ className: "min-w-6 max-h-12" }}>
+        <HeaderWidget />
+        <div className="w-full h-px bg-slate-200" />
+        <Navigation />
+        <div className="w-full h-px bg-slate-200" />
+        <Routes>
+          <Route path="/" element={<div />} />
+        </Routes>
+        <Footer />
+      </IconContext.Provider>
+    </div>
+  );
+}
+
+export default App;
